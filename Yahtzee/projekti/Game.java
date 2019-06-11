97d545ce575ab888c6e462529470c7a53809484f
ArditBaloku <arditbaloku@gmail.com>
2019-06-11 17:56:42 +0200

    Game additions

--- Yahtzee/projekti/Game.java
+++ Yahtzee/projekti/Game.java
@@ -19,6 +19,7 @@ public class Game extends BorderPane
 	private int rolls = 0;
 	private Dice dice = new Dice();
 	private ImageView face = new ImageView(dice.getFace());
+	private int stats[] = {0, 0, 0, 0, 0, 0};
 
 	public BorderPane getGameView()
 	{
@@ -59,19 +60,24 @@ public class Game extends BorderPane
 		// loja ktu
 		Button rollBtn = new Button("Roll Dice");
 		Label scoreLabel = new Label();
+		Label roundsLabel = new Label("Click the button to start playing");
 		rollBtn.setOnAction(e -> {
 			int roll = dice.roll();
 			score += roll;
+			stats[roll-1]++;
 			dice.setFace(roll);
 			face.setImage(dice.getFace());
 			rolls++;
+			scoreLabel.setText(Integer.toString(score));
+			roundsLabel.setText("You have " + (10 - rolls) + " rounds left");
 			if (rolls == 10) {
 				// insert score and player into database
-				// show game has ended message
+				// insert statistics into database
 				rolls = 0;
 				score = 0;
+				roundsLabel.setText("Game Over! Click the button to start playing again");
 			}
-			scoreLabel.setText(Integer.toString(score));
+			
 		});
 		//
 		Button signOutBtn = new Button("Sign Out");
@@ -88,6 +94,7 @@ public class Game extends BorderPane
 		pane.setCenter(face);
 		pane.setBottom(rollBtn);
 		pane.setRight(scoreLabel);
+		pane.setLeft(roundsLabel);
 		return pane;
 	}
 }
