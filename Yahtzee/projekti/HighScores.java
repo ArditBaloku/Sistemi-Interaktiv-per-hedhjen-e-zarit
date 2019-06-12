5907244b233b68f7a46398df2d478f1987128dd5
arbenamusa <43915605+arbenamusa@users.noreply.github.com>
2019-06-12 15:13:48 +0200

    now high scores are displayed

--- Yahtzee/projekti/HighScores.java
+++ Yahtzee/projekti/HighScores.java
@@ -1,7 +1,12 @@
 package projekti;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import utils.DBConnection;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
@@ -9,6 +14,7 @@ import javafx.scene.image.*;
 
 public class HighScores
 {
+	Label label2 = new Label();
 	public void getStage2()
 	{
 		Stage Stage2 = new Stage();  
@@ -16,16 +22,15 @@ public class HighScores
 		pane.setPadding(new Insets(20,20,20,20));
 		pane.setAlignment(Pos.CENTER);	
 		
+		getScores();
 		Image image = new Image("img/trofe.jpg");
 		ImageView imageView = new ImageView(image);
 		imageView.setFitHeight(150);
 		imageView.setFitWidth(150);
 		
 		Label label1 = new Label("HIGH SCORES");
-		Label label2 = new Label();
 				
 		pane.getChildren().addAll(imageView,label1,label2);
-
 				
 		Scene scene = new Scene(pane,400,400);
 			
@@ -34,4 +39,32 @@ public class HighScores
 				
 		Stage2.show();
 	}
+	private void getScores()
+	{
+		try 
+		{
+			String query = "SELECT MAX(score) FROM games_played WHERE playerId = ?";
+			
+			PreparedStatement preparedStatement = DBConnection.getConnection().prepareStatement(query);
+			preparedStatement.setInt(1, Session.getId());
+
+			ResultSet resultSet = preparedStatement.executeQuery();	
+			if(resultSet.next())
+			{
+				if(resultSet.getInt(1)>0) 
+				{
+				label2.setText(Integer.toString(resultSet.getInt(1)));
+				}
+				else
+				{
+					label2.setText("No High Scores yet!");
+				}
+			}
+		}
+		catch (SQLException ex) 
+		{
+			ex.printStackTrace();
+		}
+	}
 }
+
