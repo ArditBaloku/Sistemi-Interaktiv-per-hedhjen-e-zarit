f019c01d7484762d2ef8f45a903a97fd75bbe4e1
anzotika <43915852+anzotika@users.noreply.github.com>
2019-06-13 14:56:00 +0200

    StatusBar, HighScores

--- Yahtzee/projekti/StatusBar.java
+++ Yahtzee/projekti/StatusBar.java
@@ -7,17 +7,20 @@ import utils.Session;
 
 public class StatusBar extends HBox
 {
+	private Label player;
+
 	public HBox getStatusBar()
 	{
+		player = I18N.getLabel("label15");
 		HBox statusBar = new HBox(10);
 		Label userInfo = new Label();
 		userInfo.setPadding(new Insets(5,20,5,5));
 		String fullName = Session.getFullName();
-		userInfo.setText("Player: " + fullName);
+		userInfo.setText(player.getText() + fullName);
 		userInfo.setStyle("-fx-text-fill:black;");
 		statusBar.getChildren().add(userInfo);
 		statusBar.setStyle("-fx-background-color:linear-gradient(#c2b6b6, #576574);");
-		
+
 		return statusBar;
 	}
 }
